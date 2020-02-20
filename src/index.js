ac13c66e4620988aac532e8a19113a432cad9e16
Mathieu Lussier <mathilekiki@hotmail.com>
2020-02-20 18:13:39 -0500

    cleaning index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
 'use strict';
 
-const LOAD_ONLINE = true;
-// if (LOAD_ONLINE) {
-//     require('./connection');
-// }
+const LOAD_ONLINE = false;
 const net = require('net');
 const board = require('./board');
 const pawn = require('./pawn');
 
-const client = net.createConnection({port: 17017, host: 'csi.cstjean.qc.ca'}, function() {
-    console.log('Connected');
-});
+if (LOAD_ONLINE) {
+    const client = net.createConnection({port: 17017, host: 'csi.cstjean.qc.ca'}, function() {
+        console.log('Connected');
+    });
 
-client.on('data', function(data) {
-    board.getTcpData(data.toString());
-});
+    client.on('data', function(data) {
+        board.getTcpData(data.toString());
+    });
 
-setTimeout(() => {
-    main();
-}, 1700);
+    setTimeout(() => {
+        main();
+    }, 1700);
 
-client.on('error', function(err) { throw err });
+    client.on('error', function(err) { throw err });
 
-client.on('close', function() {
-    console.log('Connection closed');
-});
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
 
-client.on('end', function() {
-    console.log('disconnected from server');
-});
+    client.on('end', function() {
+        console.log('disconnected from server');
+    });
+}
 
 async function main() {
     console.time('RunTime');
